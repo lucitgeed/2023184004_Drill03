48a2481a6e5e7b290649235f5cae1f37805f61c2
homedesktop@2DGP <gunageroo@gmail.com>
2024-09-24 12:33:59 +0900

    사각형 윗줄 이동 완성(top)

--- drill#2_character_move.py
+++ drill#2_character_move.py
@@ -9,9 +9,19 @@ grass = load_image('grass.png')
 char = load_image('character.png')
 
 
+def draw_char(x, y):
+        clear_canvas_now()
+        char.draw_now(x, y)
+        delay(0.1)
+
+    
 
 def run_top():
     print("top")
+    for x in range(0, 800, 10):
+        draw_char(x, 550)
+
+        
     pass
 
 def run_right():
@@ -53,9 +63,9 @@ def run_circle():
         theta = math.radians(degree)
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
-        clear_canvas_now()
-        char.draw_now(x, y)
-        delay(0.1)
+        
+        draw_char(x, y)
+
 
     pass
     
@@ -64,7 +74,7 @@ def run_circle():
 
 while True:
     run_rectangle()
-    # run_circle()
+    #run_circle()
 
     break
 
