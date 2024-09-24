e96b7bd795b22cef7d3a6b7f6a36efdf06bd5149
homedesktop@2DGP <gunageroo@gmail.com>
2024-09-24 11:50:00 +0900

    예전내용 지우고 뼈대 재구성

--- drill#2_character_move.py
+++ drill#2_character_move.py
@@ -2,78 +2,31 @@ from pico2d import *
 
 open_canvas()
 
+
+
 grass = load_image('grass.png')
 char = load_image('character.png')
 
 
-grass.draw_now(400, 30)
-char.draw_now(400, 90)
-
-
-while(True):
-
-    for i in range(0, 200):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        char.draw_now(400 + i, 90)
-        i = i + 2
-
-    for i in range(0, 200):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        char.draw_now(400 + 200, 90 + i)
-        i = i+2
-
-    for i in range(0,400):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        char.draw_now(400 + 200 - i, 90 + 200)
-        i = i+2
-
-    for i in range(0, 200):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        char.draw_now(400 + 200 - 400, 90 + 200 - i)
-        i = i + 2
-
-    for i in range(0, 200):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        char.draw_now(400 + 200 - 400 + i, 90)
-        i = i + 2
-
-
-    delay(2)
+def run_rectangle():
+    print("ractangle")
+    pass
 
 
 
-    for x in range(0, 50):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            char.draw_now(400 + x, 90 + x)
-            x = x + 2
-
-    for x in range(0, 50):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            char.draw_now(400 + 50 - x, 90 + 50 + x)
-            x = x + 2
 
 
-    for x in range(0, 50):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            char.draw_now(400 - x, 90 + 50 + 50 - x)
-            x = x + 2
+
+def run_circle():
+    print("circle")
+    pass
+    
 
 
-    for x in range(0, 50):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            char.draw_now(400 - 50 + x, 90 + 50 - x)
-            x = x + 2
 
-    delay(2)
+while True:
+    run_rectangle()
+    run_circle()
 
 
 
